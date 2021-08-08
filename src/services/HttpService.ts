cf59c7afc1c69d730522021c6570e73eed0e5fb5
Jonathan Fridja <johnfridja@gmail.com>
2021-08-08 22:00:28 +0300

    added error logging

--- src/services/HttpService.ts
+++ src/services/HttpService.ts
@@ -10,6 +10,20 @@ axios.interceptors.request.use((axiosRequest) => {
         console.log(axiosRequest);
     }
     return axiosRequest;
-})
+
+}, requestError => {
+    if (process.env.NODE_ENV !== 'production') {
+        console.error(requestError);
+    }
+    return Promise.reject(requestError);
+});
+
+axios.interceptors.response.use(undefined, responseError => {
+    if (process.env.NODE_ENV !== 'production') {
+        console.error(responseError);
+    }
+    return Promise.reject(responseError);
+});
+
 
 export const httpService = axios;
