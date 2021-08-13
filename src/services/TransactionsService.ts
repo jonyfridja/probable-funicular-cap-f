94919929913dd3d96a239621f3fd8800544a25c6
Jonathan Fridja <johnfridja@gmail.com>
2021-08-13 12:19:59 +0300

    compress logic in transactionsService

--- src/services/TransactionsService.ts
+++ src/services/TransactionsService.ts
@@ -1,4 +1,3 @@
-import { USER_NAME } from "../constants";
 import { ITransaction } from "../interfaces";
 import { httpService } from "./HttpService";
 
@@ -11,10 +10,45 @@ class TransactionsService {
         return httpService.post<ITransaction>('/transactions', transaction).then(res => res.data)
     }
 
-    async compress(transactions: ITransaction[]) {
-        return httpService.post<ITransaction[]>('/transactions/compress', transactions).then(res => res.data)
+    compress = (transactions: ITransaction[]) => {
+        function makeKeysStable(_transactions: ITransaction[]) {
+            return _transactions.map(t => {
+                if (t.tradingparty < t.counterparty) {
+                    return {
+                        ...t,
+                        counterparty: t.tradingparty,
+                        tradingparty: t.counterparty,
+                        amount: t.amount * -1
+                    }
+                }
+                return t
+            })
+        }
+
+        const stableKeyedTransactions = makeKeysStable(transactions)
+
+        const delimiter = '__@@__';
+        let res = stableKeyedTransactions.reduce((acc, t) => {
+            let key = t.tradingparty + delimiter + t.counterparty;
+            if (acc[key]) acc[key].amount += t.amount;
+            else acc[key] = { key, amount: t.amount }
+            return acc
+        }, {} as Record<string, { key: string, amount: number }>);
+
+        let compressed = []
+        for (const key in res) {
+            const curr = res[key]
+            const [tradingparty, counterparty] = key.split(delimiter);
+            compressed.push({
+                counterparty,
+                tradingparty,
+                amount: curr.amount
+            })
+        }
+
+        return compressed
     }
-    
+
 }
 
 export const transactionsService = new TransactionsService();
