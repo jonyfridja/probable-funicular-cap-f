0ea1412ad0a84288df57185ae4cd4a71d809015e
Jonathan Fridja <johnfridja@gmail.com>
2021-08-13 13:32:51 +0300

    validation

--- src/pages/TransactionPage/TransactionForm/TransactionForm.tsx
+++ src/pages/TransactionPage/TransactionForm/TransactionForm.tsx
@@ -9,10 +9,11 @@ interface Props {
     isOpen: boolean;
     onClose: Function
 }
+
 export default function TransactionForm({ onSubmit, disabled, ...props }: Props): ReactElement {
     const [counterparty, setCounterParty] = useState<string>('');
     const [amount, setAmount] = useState(0)
-
+    const [validateErrorMessage, setValidateErrorMessage] = useState('')
     const clearForm = () => {
         setAmount(0);
         setCounterParty('')
@@ -21,27 +22,44 @@ export default function TransactionForm({ onSubmit, disabled, ...props }: Props)
         clearForm()
         props.onClose();
     }
-    
-    
+
+    const validate = () => {
+        let valid = false;
+        if (!counterparty) {
+            setValidateErrorMessage('Recipient is required')
+            return valid
+        }
+
+        if (amount === 0) {
+            setValidateErrorMessage('Please set Amount')
+            return valid
+        }
+
+        return true
+    }
+
     return (
         <Modal open={props.isOpen}>
             <OutsideAlerter onOutsideClicked={onClose}>
                 <div className="transaction-form">
                     <form className="modal-body" onSubmit={(ev) => {
                         ev.preventDefault();
+                        if (!validate()) return
+                        
                         onSubmit({ counterparty, amount });
                         clearForm()
                     }}>
                         <div className="close-btn" onClick={() => onClose()}>X</div>
                         <label>
                             Recipient
-                            <input type="text" onChange={({ target }) => setCounterParty(target.value)} value={counterparty} />
+                            <input required type="text" onChange={({ target }) => setCounterParty(target.value)} value={counterparty} />
                         </label>
                         <label>
                             How much?
-                            <input type="number" onChange={({ target }) => setAmount(+target.value)} value={amount} />
+                            <input required type="number" onChange={({ target }) => setAmount(+target.value)} value={amount} />
                         </label>
-                        <button className="app-button" disabled={disabled}>Add</button>
+                        {validateErrorMessage && <div className="validate-error">{validateErrorMessage}</div>}
+                        <button type="submit" className="app-button" disabled={disabled}>Add</button>
                     </form>
                 </div>
             </OutsideAlerter>
